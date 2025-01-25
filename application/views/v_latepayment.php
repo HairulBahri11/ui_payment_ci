039b03578bc6a9a227574e0c48a0e0f88c23ac84
Hairul Bahri <80257426+HairulBahri11@users.noreply.github.com>
2025-01-25 22:39:10 +0700

    dateformat

--- application/views/v_latepayment.php
+++ application/views/v_latepayment.php
@@ -323,11 +323,13 @@ $(document).ready(function(){
             return student.id == id;
         });
         var updatedPhone = $('input[name="phone_' + id + '"]').val();
+        var studentMonthPay = new Date(student.monthpay).toLocaleDateString('en-US', { month: 'short', year: 'numeric' });
+        
         return {
             id: student.id,
             name: student.name,
             phone: updatedPhone, // Perbarui phone dengan input terbaru
-            lastpaydate: student.monthpay,
+            lastpaydate: studentMonthPay,
         };
     }
 });
